5bb33a5342a4f5d22a40cb52a216e5d19e4e53be
Rahul <6206472735rahul@gmail.com>
2025-01-30 14:17:40 +0530

    Added some api like findStudentById,findAllStudent,CountAllStudent..

--- src/main/java/com/JpaHibernate/student_management/controller/StudentController.java
+++ src/main/java/com/JpaHibernate/student_management/controller/StudentController.java
@@ -3,10 +3,10 @@ package com.JpaHibernate.student_management.controller;
 import com.JpaHibernate.student_management.model.Student;
 import com.JpaHibernate.student_management.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student/api")
@@ -20,4 +20,27 @@ public class StudentController {
        String response=  studentService.addStudent(studentRequest);
        return response;
     }
+
+    // create a api for findStudentById using get method
+    @GetMapping("/find/{id}")
+    public Student findStudentById(@PathVariable int id){
+     Student response= studentService.findStudentById(id);
+     return response;
+
+    }
+
+    // create a api for find all student using get method
+    @GetMapping("/findAll")
+    public List<Student> findAllStudent(){
+        List<Student>studentList= studentService.findAllStudent();
+       return studentList;
+    }
+    // create a CountAllStudent api using get method
+    @GetMapping("/count")
+    public long CountAllStudent(){
+      long response= studentService.CountAllStudent();
+      return response;
+
+    }
+
 }
--- src/main/java/com/JpaHibernate/student_management/service/StudentService.java
+++ src/main/java/com/JpaHibernate/student_management/service/StudentService.java
@@ -4,6 +4,10 @@ import com.JpaHibernate.student_management.model.Student;
 import com.JpaHibernate.student_management.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service // it's a service layer and contains business logic
 public class StudentService {
@@ -11,13 +15,37 @@ public class StudentService {
     @Autowired // it helps to create obj for jpa child class
     // here we call child class like StudentRepository
     private StudentRepository studentRepository;
+
     // create a method for addStudent so that we can
     // use jpa inbuilt fun here
-    public String addStudent(Student studentRequest){
+    public String addStudent(Student studentRequest) {
         studentRepository.save(studentRequest);
         return "Student saved successfully in db";
     }
+    // find student details with help of findById()
+    public Student findStudentById(int studentId){
+        Optional<Student> studentOptional= studentRepository.findById(studentId);
+        if(studentOptional.isPresent()){
+         return studentOptional.get();
+        }
+       return null;
+    }
 
+    // find all student using get method
+    public List<Student> findAllStudent(){
+       List<Student> Studentlist= studentRepository.findAll();
+       return Studentlist;
 
+    }
 
+    // count all student whatever present in db
+    public long CountAllStudent(){
+      long response=  studentRepository.count();
+      return response;
+    }
+    //
 }
+
+
+
+
